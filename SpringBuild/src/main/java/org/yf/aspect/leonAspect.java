799ef849904f9e50a1b451aca73241735511fd57
yangfei <56>
2023-02-09 03:23:12 +0800

    基本类提交

---
+++ SpringBuild/src/main/java/org/yf/aspect/leonAspect.java
@@ -0,0 +1,10 @@
+//package org.yf.aspect;
+//
+//import org.aopalliance.intercept.Joinpoint;
+//
+//public class leonAspect {
+//    @Before("")
+//    public void leonBefore(Joinpoint joinpoint){
+//        System.out.println("leonBefore");
+//    }
+//}
