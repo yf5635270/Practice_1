7b291b9cea1cec85c3ce5a7901d60aaae1800948
yangfei <56>
2023-02-15 11:34:48 +0800

    redis配置和基本使用

--- YFSpringBoot/src/main/java/com/yf/service/JdbcTemplaterService.java
+++ YFSpringBoot/src/main/java/com/yf/service/JdbcTemplaterService.java
@@ -1,6 +1,7 @@
 package com.yf.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +12,15 @@ public class JdbcTemplaterService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+//    @Autowired
+//    private RedisTemplate redisTemplate;
+
     public void test(){
         jdbcTemplate.execute("insert into user(name,age,gender) values('yf',40,1)");
     }
+
+//    public void redisTest(){
+//        redisTemplate.opsForValue().set("name","yangfei");
+//        redisTemplate.opsForValue().get("name");
+//    }
 }
